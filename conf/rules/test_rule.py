486e8a460feda19d5da15902051e6b19d4337102
spacemanspiff2007 <->
2019-02-08 08:43:07 +0100

    Added MqttTestRule

--- conf/rules/test_rule.py
+++ conf/rules/test_rule.py
@@ -1,8 +1,31 @@
 import time, datetime, random
 import HABApp
-#import HABApp.
 from HABApp.core.events import ValueChangeEvent, ValueUpdateEvent
-from HABApp.openhab.events.item_events import ItemStateEvent
+from HABApp.openhab.events import ItemStateEvent
+
+
+class MqttTestRule(HABApp.Rule):
+
+    def __init__(self):
+        super().__init__()
+        self.mqtt_publish('test/test', 'Starting MqttTestRule')
+
+        self.run_every(
+            datetime.datetime.now() + datetime.timedelta(seconds=10),
+            interval=datetime.timedelta(seconds=20),
+            callback=self.publish_rand_value
+        )
+
+        self.listen_event('test/test', self.topic_changed)
+
+    def publish_rand_value(self):
+        print('test mqtt_publish')
+        self.mqtt_publish('test/test', str(random.randint(0,1000)))
+
+    def topic_changed(self, event):
+        assert isinstance(event, ValueUpdateEvent), type(event)
+        print( f'mqtt topic "test/test" changed to {event.value}')
+
 
 class MyRule(HABApp.Rule):
 
@@ -22,14 +45,15 @@ class MyRule(HABApp.Rule):
         self.__b_val = random.randint(0,9999)
 
 
-        #self.run_at(datetime.datetime.now(), self.prepare_bench)
+        self.run_at(datetime.datetime.now(), self.prepare_bench)
 
     def prepare_bench(self):
         for k in self.item_list:
             self.create_openhab_item('string', k)
 
+        time.sleep(2)
         self.run_minutely(self.bench_start)
-        self.listen_event(self.item_list[-1], self.bench_stop, None)
+        self.listen_event(self.item_list[-1], self.bench_stop, ValueChangeEvent)
 
 
     def print_ts(self, arg, asdf = None):
@@ -69,9 +93,6 @@ class MyRule(HABApp.Rule):
     def cb(self, event):
         print( f'CALLBACK: {event}')
         assert isinstance(event, ValueUpdateEvent)
-        # self.post_Update('MyOtherItem', 'test')
-        self.mqtt_publish( 'test/topic1', event.value + 1)
-
 
         #time.sleep(0.6)
 
@@ -85,7 +106,9 @@ class MyRule(HABApp.Rule):
 
 
 a = MyRule()
-# MyRule()
+b = MyRule()
+
+
 
 
 
