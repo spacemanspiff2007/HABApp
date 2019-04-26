a12525fa1aa866768d9eaf788123e7b113d57d79
spacemanspiff2007 <accounts@sgerber.de>
2019-04-26 09:24:33 +0200

    added logging example

---
+++ conf/rules/logging_rule.py
@@ -0,0 +1,20 @@
+import logging
+
+import HABApp
+
+log = logging.getLogger('MyRule')
+
+
+class MyLoggingRule(HABApp.Rule):
+
+    def __init__(self):
+        super().__init__()
+
+        # different levels are available
+        log.debug('Debug Message')
+        log.info('Info Message')
+        log.warning('Warning Message')
+        log.error('Error Message')
+
+
+MyLoggingRule()
