bf814e6b2404292ab8ddaa08a394203ba053a119
spacemanspiff2007 <accounts@sgerber.de>
2019-09-12 10:46:23 +0200

    Added conf_testing to git

---
+++ conf_testing/rules/test_parameter_files.py
@@ -0,0 +1,30 @@
+import logging
+
+import HABApp
+from HABAppTests import TestBaseRule
+
+log = logging.getLogger('HABApp.TestParameterFiles')
+
+# User Parameter files to create rules dynamically
+try:
+    assert HABApp.parameters.get_value('param_file', 'key') != 10, f'Loading of Parameters does not work properly'
+except Exception as e:
+    log.error(e)
+
+
+class TestParamFile(TestBaseRule):
+    """This rule is testing MQTT by posting values and checking the events"""
+
+    def __init__(self):
+        super().__init__()
+
+        self.add_test('ParamFile', self.test_param_file)
+
+    def test_param_file(self):
+        p = HABApp.parameters.get_parameter('param_file', 'key')
+        assert p < 11
+        assert p.value == 10
+        return True
+
+
+TestParamFile()
