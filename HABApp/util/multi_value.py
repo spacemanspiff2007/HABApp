979f7ca24af97d79780585b0c202e2e10419dbb5
spacemanspiff2007 <accounts@sgerber.de>
2019-09-22 15:53:12 +0200

    Reworked MultiValue as MultiValueItem

--- HABApp/util/multi_value.py
+++ HABApp/util/multi_value.py
@@ -1,16 +1,29 @@
 import datetime
-import typing
+import typing, logging
+import operator
 from threading import Lock
+from HABApp.core.items import Item
 
 
 class ValueWithPriority:
-    def __init__(self, parent, initial_value=None):
+    DISABLE_OPERATORS = {
+        '>': operator.gt, '<': operator.lt, '>=': operator.ge, '<=': operator.le, '==': operator.eq, None: None
+    }
+    
+    def __init__(self, parent, name:str, initial_value=None, auto_disable_on=None, auto_disable_after=None):
 
-        assert isinstance(parent, MultiValue), type(parent)
-        self.__parent: MultiValue = parent
-
+        assert isinstance(parent, MultiValueItem), type(parent)
+        assert isinstance(name, str), type(name)
+        self.__parent: MultiValueItem = parent
+        self.__name = name
+        
         self.__value = None
         self.__enabled = False
+        
+        assert isinstance(auto_disable_after, datetime.timedelta) or auto_disable_after is None, type(auto_disable_after)
+        assert auto_disable_on in ValueWithPriority.DISABLE_OPERATORS, auto_disable_on
+        self.auto_disable_after: datetime.timedelta = auto_disable_after
+        self.auto_disable_on: str = auto_disable_on
 
         #: Timestamp of the last update/enable of this value
         self.last_update: datetime.datetime = datetime.datetime.now()
@@ -40,7 +53,10 @@ class ValueWithPriority:
 
         self.last_update = datetime.datetime.now()
 
-        self.__parent.recalculate_value(self)
+        if self.__parent.log is not None:
+            self.__parent.log.info(f'{self.__parent.name}: {self.__name} set value to {self.__value}')
+
+        self.__parent.recalculate_value()
 
     def set_enabled(self, value: bool):
         """Enable or disable this value and recalculate overall value
@@ -52,7 +68,36 @@ class ValueWithPriority:
 
         self.last_update = datetime.datetime.now()
 
-        self.__parent.recalculate_value(self)
+        self.__parent.recalculate_value()
+
+    def calculate_value(self, lower_value):
+
+        # so we don't spam the log if we are already disabled
+        if not self.__enabled:
+            return lower_value
+
+        # Automatically disable after certain time
+        if self.auto_disable_after is not None:
+            if datetime.datetime.now() > self.last_update + self.auto_disable_after:
+                self.__enabled = True
+                self.last_update = datetime.datetime.now()
+                if self.__parent.log is not None:
+                    self.__parent.log.info(f'{self.__parent.name}: {self.__name} disabled '
+                                           f'(after {self.auto_disable_after})!')
+        
+        # Automatically disable if <> etc.
+        if self.auto_disable_on is not None:
+            if ValueWithPriority.DISABLE_OPERATORS[self.auto_disable_on](lower_value, self.__value):
+                self.__enabled = False
+                self.last_update = datetime.datetime.now()
+                if self.__parent.log is not None:
+                    self.__parent.log.info(f'{self.__parent.name}: {self.__name} disabled '
+                                           f'({lower_value}{self.auto_disable_on}{self.__value})!')
+
+        if not self.__enabled:
+            return lower_value
+
+        return self.__value
 
     def __str__(self):
         return str(self.__value)
@@ -61,42 +106,50 @@ class ValueWithPriority:
         return f'<{self.__class__.__name__} enabled: {self.__enabled}, value: {self.__value}>'
 
 
-class MultiValue:
+class MultiValueItem(Item):
     """Thread safe value prioritizer"""
 
-    def __init__(self, on_value_change=None):
-        """
+    def __init__(self, name: str, state=None):
+        super().__init__(name=name, state=state)
 
-        :param on_value_change: Callback with one arg which will be called on every change
-        """
-        self.on_value_change = on_value_change
-
-        self.__value = None
-
-        self.__children: typing.Dict[int, ValueWithPriority] = {}
+        self.__values_by_prio: typing.Dict[int, ValueWithPriority] = {}
+        self.__values_by_name: typing.Dict[str, ValueWithPriority] = {}
+        
         self.__lock = Lock()
+        
+        self.log: logging._loggerClass = None
 
-    @property
-    def value(self):
-        """Returns the current value"""
-        return self.__value
-
-    def get_create_value(self, priority: int, initial_value=None) -> ValueWithPriority:
-        """ Create a new instance which can be used to set values
-
-        :param priority: priority of the value
-        :param initial_value: initial value
-        """
-        assert isinstance(priority, int), type(priority)
-
+    def add_multivalue(self, name: str, priority: int,
+                       initial_value=None, auto_disable_on=None, auto_disable_after=None):
+        # Silently overwrite the values
+        # assert not name.lower() in self.__values_by_name, name.lower()
+        # assert not priority in self.__values_by_prio, priority
+        
         with self.__lock:
-            if priority in self.__children:
-                return self.__children[priority]
+            ret = ValueWithPriority(
+                self, name,
+                initial_value=initial_value, auto_disable_on=auto_disable_on, auto_disable_after=auto_disable_after
+            )
+            self.__values_by_prio[priority] = ret
+            self.__values_by_name[name.lower()] = ret
+        return ret
 
-            self.__children[priority] = ret = ValueWithPriority(self, initial_value)
-            return ret
+    def get_multivalue(self, name: str):
+        return self.__values_by_name[name.lower()]
 
-    def recalculate_value(self, child):
+    def get_value_until(self, child_to_stop):
+        assert isinstance(child_to_stop, ValueWithPriority), type(child_to_stop)
+        new_value = None
+        with self.__lock:
+            for priority, child in sorted(self.__values_by_prio.items()):
+                if child is child_to_stop:
+                    return new_value
+                
+                assert isinstance(child, ValueWithPriority)
+                new_value = child.calculate_value(new_value)
+        raise ValueError()
+
+    def recalculate_value(self):
         """Recalculate the output value and call the registered callback (if output has changed)
 
         :param child: child that changed
@@ -105,20 +158,12 @@ class MultiValue:
 
         # recalculate value
         new_value = None
-
         with self.__lock:
-            for priority, child in sorted(self.__children.items()):
+            for priority, child in sorted(self.__values_by_prio.items()):
                 assert isinstance(child, ValueWithPriority)
-
-                if not child.enabled:
-                    continue
-                new_value = child.value
-
-            value_changed = new_value != self.__value
-            self.__value = new_value
+                new_value = child.calculate_value(new_value)
 
         # Notify that the value has changed
-        if value_changed and self.on_value_change is not None:
-            self.on_value_change(new_value)
-
+        if new_value is not None:
+            self.post_state(new_value)
         return new_value
--- tests/test_utils/test_multivalue.py
+++ tests/test_utils/test_multivalue.py
@@ -1,46 +1,23 @@
-from unittest.mock import MagicMock
-
-from HABApp.util import MultiValue
-
-
-def test_same_priority():
-
-    on_change_func = MagicMock()
-
-    p = MultiValue(on_value_change=on_change_func)
-    a1 = p.get_create_value(0, '1234')
-    a2 = p.get_create_value(0, '1234')
-
-    a2.set_value(1)
-    assert p.value == 1
-    on_change_func.assert_called_once_with(1)
-
-    a1.set_value(2)
-    assert p.value == 2
-    on_change_func.assert_called_with(2)
-
+from HABApp.util import MultiValueItem
 
 def test_diff_prio():
+    p = MultiValueItem('TestItem')
+    p.add_multivalue('modea', 1, '1234')
+    p.add_multivalue('modeb', 2, '4567')
 
-    on_change_func = MagicMock()
-
-    p = MultiValue(on_value_change=on_change_func)
-    p1 = p.get_create_value(1, '1234')
-    p2 = p.get_create_value(2, '4567')
-
+    p1 = p.get_multivalue('modea')
+    p2 = p.get_multivalue('modeb')
+    
     p1.set_value(5)
-    assert p.value == '4567'
-    on_change_func.assert_called_with('4567')
+    assert p.state == '4567'
 
     p2.set_enabled(False)
-    assert p.value == 5
-    on_change_func.assert_called_with(5)
+    assert p.state == 5
 
     p2.set_enabled(True)
-    assert p.value == '4567'
-    on_change_func.assert_called_with('4567')
+    assert p.state == '4567'
 
     p2.set_enabled(False)
     p2.set_value(8888)
-    assert p.value == 8888
-    on_change_func.assert_called_with(8888)
+    assert p.state == 8888
+
