839dc5f8da9ba988d86dc4be55eb52f4e6394b50
spacemanspiff2007 <accounts@sgerber.de>
2020-03-25 06:38:32 +0100

    files and folders are loaded in alphabetical order

--- HABApp/runtime/folder_watcher/habappfileevent.py
+++ HABApp/runtime/folder_watcher/habappfileevent.py
@@ -28,10 +28,12 @@ class FileEventToHABAppEvent(SimpleAsyncEventHandler):
             self.habapp_topic, event.from_path(self.folder, path)
         )
 
+    @HABApp.core.wrapper.log_exception
     def trigger_load_for_all_files(self, delay: int = None):
 
-        # trigger event for every file
-        for f in self.folder.glob(f'**/*{self.file_ending}' if self.recursive else f'*{self.file_ending}'):
+        # trigger event for every file, we load in alphabetical order
+        rule_files = self.folder.glob(f'**/*{self.file_ending}' if self.recursive else f'*{self.file_ending}')
+        for f in sorted(rule_files, key=lambda x: x.relative_to(self.folder)):
             if not f.name.endswith(self.file_ending):
                 continue
 
