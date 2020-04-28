42a00d7617633d7fdb7e2d023e0fee0121191806
spacemanspiff2007 <accounts@sgerber.de>
2020-04-28 12:00:40 +0200

    fixed missing LinkNotFoundError

--- HABApp/openhab/definitions/rest/links.py
+++ HABApp/openhab/definitions/rest/links.py
@@ -7,3 +7,7 @@ class ItemChannelLinkDefinition(BaseModel):
     item_name: str = Field(alias='itemName')
     channel_uid: str = Field(alias='channelUID')
     configuration: Dict[str, Any] = {}
+
+
+class LinkNotFoundError(Exception):
+    pass
