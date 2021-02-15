192e3aeb22e091c75cf6d97fd5b91ca31558cd6f
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2021-02-15 06:49:25 +0100

    - Fixed a bug where config was not reloaded when HABApp was running - Added possibility to add own logging levels - Frontail should work now on openhabian

--- HABApp/config/default_logfile.py
+++ HABApp/config/default_logfile.py
@@ -4,10 +4,10 @@ from .platform_defaults import get_log_folder, is_openhabian
 
 def get_default_logfile() -> str:
     template = Template("""
-formatters:
+${LOG_LEVELS}formatters:
   HABApp_format:
-    format: ${LOG_FORMAT}
-
+    format: '[%(asctime)s] [%(name)25s] %(levelname)8s | %(message)s'
+${FRONTAIL_FORMAT}
 
 handlers:
   # There are several Handlers available:
@@ -24,7 +24,7 @@ handlers:
     maxBytes: 1_048_576
     backupCount: 3
 
-    formatter: HABApp_format
+    formatter: ${HABAPP_FILE_FORMAT}
     level: DEBUG
 
   EventFile:
@@ -63,7 +63,9 @@ loggers:
     subs = {
         'EVENT_FILE': 'events.log',
         'HABAPP_FILE': 'HABApp.log',
-        'LOG_FORMAT': "'[%(asctime)s] [%(name)25s] %(levelname)8s | %(message)s'",
+        'HABAPP_FILE_FORMAT': 'HABApp_format',
+        'FRONTAIL_FORMAT': '',
+        'LOG_LEVELS': '',
     }
 
     # Use abs path and rename events.log if we log in the openhab folder
@@ -75,9 +77,16 @@ loggers:
         # Keep this relative so it is easier to read in the file
         subs['EVENT_FILE'] = 'HABApp_events.log'
 
-    # prepare fronttail integration for openhabian
-    if is_openhabian():
-        subs['LOG_FORMAT'] = "'%(asctime)s.%(msecs)03d [%(levelname)-5s] [%(name)-36s] - %(message)s'\n    " \
-                             "datefmt: '%Y-%m-%d %H:%M:%S'"
+        # With openhabian we typically use frontail
+        if is_openhabian():
+            subs['FRONTAIL_FORMAT'] = '\n'\
+                '  Frontail_format:\n' \
+                "    format: '%(asctime)s.%(msecs)03d [%(levelname)-5s] [%(name)-36s] - %(message)s'\n" \
+                "    datefmt: '%Y-%m-%d %H:%M:%S'"
+    
+            subs['HABAPP_FILE_FORMAT'] = 'Frontail_format'
+    
+            # frontail expects WARN instead of WARNING
+            subs['LOG_LEVELS'] = 'levels:\n  WARNING: WARN\n\n'
 
     return template.substitute(**subs)
--- tests/test_config/test_platform.py
+++ tests/test_config/test_platform.py
@@ -10,8 +10,25 @@ def test_defaults():
 
 
 def test_valid_yml(monkeypatch):
+    """ensure we create valid yml files"""
+    
+    def ensure_key(key, obj):
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                ensure_key(k, v)
+        else:
+            assert obj != ''
+
     monkeypatch.setattr(default_logfile, 'is_openhabian', lambda: True)
     monkeypatch.setattr(default_logfile, 'get_log_folder', lambda: Path('/platfrom/log/folder'))
 
     default = default_logfile.get_default_logfile()
-    config_loader._yaml_param.load(default)
+    ensure_key('root', config_loader._yaml_param.load(default))
+
+    monkeypatch.setattr(default_logfile, 'is_openhabian', lambda: False)
+    default = default_logfile.get_default_logfile()
+    ensure_key('root', config_loader._yaml_param.load(default))
+
+    monkeypatch.setattr(default_logfile, 'get_log_folder', lambda: None)
+    default = default_logfile.get_default_logfile()
+    ensure_key('root', config_loader._yaml_param.load(default))
