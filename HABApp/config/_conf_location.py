597122887f8d79d46f55f6e34e54de2fb890cf19
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2021-04-09 14:12:24 +0200

    removed astral

--- HABApp/config/_conf_location.py
+++ HABApp/config/_conf_location.py
@@ -1,7 +1,6 @@
 import logging.config
 
-import astral as _astral
-import tzlocal
+import eascheduler
 import voluptuous
 from EasyCo import ConfigContainer, ConfigEntry
 
@@ -16,19 +15,6 @@ class Location(ConfigContainer):
     def __init__(self):
         super().__init__()
 
-        self._astral_location: _astral.LocationInfo
-        self.astral_observer: _astral.Observer
-
     def on_all_values_set(self):
-        tz = tzlocal.get_localzone()
-        tz_name = str(tz)
-        log.debug(f'Local Timezone: {tz_name}')
-
-        # unsure why we need the location in 2.1
-        self._astral_location = _astral.LocationInfo(name='HABApp', )
-        self._astral_location.latitude = self.latitude
-        self._astral_location.longitude = self.longitude
-        self._astral_location.timezone = tz_name
-
-        self.astral_observer = self._astral_location.observer
-        self.astral_observer.elevation = self.elevation
+        log.debug(f'Local Timezone: {eascheduler.const.local_tz}')
+        eascheduler.set_location(self.longitude, self.longitude, self.elevation)
