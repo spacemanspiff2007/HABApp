bf17ce83a59f057e6a99a33ccd7c83d67dbe0c19
Dominik Bernhardt <dominik.bernhardt@stryker.com>
2021-04-10 22:52:51 +0200

    Fix DateTime string parsing for OH 3.1

--- HABApp/openhab/map_items.py
+++ HABApp/openhab/map_items.py
@@ -61,7 +61,13 @@ def map_item(name, openhab_type: str, openhab_value: str) -> typing.Optional[Bas
         if openhab_type == "DateTime":
             if value is None:
                 return DatetimeItem(name, value)
-            dt = datetime.datetime.strptime(value.replace('+', '000+'), '%Y-%m-%dT%H:%M:%S.%f%z')
+            # Previous OH versions used a datetime string like this:
+            # 2018-11-19T09:47:38.284+0100
+            # OH 3.1 uses
+            # 2021-04-10T22:00:43.043996+0200 
+            if len(value)==28:
+                value = value.replace('+', '000+')
+            dt = datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f%z')
             # all datetimes from openhab have a timezone set so we can't easily compare them
             # --> TypeError: can't compare offset-naive and offset-aware datetimes
             dt = dt.astimezone(tz=None)   # Changes datetime object so it uses system timezone
