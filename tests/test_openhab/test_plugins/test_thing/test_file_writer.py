c3649ddfaaeb1bd89031a81b0dd04db9ac238b43
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2021-05-07 05:56:56 +0200

    fixed missing " for tags in textual thing configuration

--- tests/test_openhab/test_plugins/test_thing/test_file_writer.py
+++ tests/test_openhab/test_plugins/test_thing/test_file_writer.py
@@ -41,10 +41,10 @@ def test_creation(tmp_path_factory):
 
     # print('\n' + '-' * 120 + '\n' + t.text + '-' * 120)
 
-    expected = """String  Test_zwave_o_1                                                {channel = "zwave:link:device"                        }
-String  Test_zwave_o_2                                                {channel = "zwave:link:device1",   auto_update="False"}
-String  Test_zwave_no                                 [tag1]
-String  Test_zwave_all  "label1"    <icon1>   (grp1)  [tag1, tag2]
+    expected = """String  Test_zwave_o_1                                                    {channel = "zwave:link:device"                        }
+String  Test_zwave_o_2                                                    {channel = "zwave:link:device1",   auto_update="False"}
+String  Test_zwave_no                                 ["tag1"]
+String  Test_zwave_all  "label1"    <icon1>   (grp1)  ["tag1", "tag2"]
 
 String  NewName     {auto_update="False"}
 String  NewName1    {auto_update="False"}
