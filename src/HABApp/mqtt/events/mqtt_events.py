e8afe7712a1c546224bc90f18e7582fc4bbae47f
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2021-12-22 10:37:28 +0100

    -

--- src/HABApp/mqtt/events/mqtt_events.py
+++ src/HABApp/mqtt/events/mqtt_events.py
@@ -3,9 +3,9 @@ import HABApp
 
 class MqttValueUpdateEvent(HABApp.core.events.ValueUpdateEvent):
     # Copy the annotations, otherwise the code won't work from py3.10 on
-    __annotations__ = HABApp.core.events.ValueUpdateEvent.__annotations__
+    __annotations__ = HABApp.core.events.ValueUpdateEvent.__annotations__.copy()
 
 
 class MqttValueChangeEvent(HABApp.core.events.ValueChangeEvent):
     # Copy the annotations, otherwise the code won't work from py3.10 on
-    __annotations__ = HABApp.core.events.ValueChangeEvent.__annotations__
+    __annotations__ = HABApp.core.events.ValueChangeEvent.__annotations__.copy()
