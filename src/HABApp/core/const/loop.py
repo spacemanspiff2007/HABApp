68a95334703a8416c0913e185488080b41db8026
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2023-01-09 15:39:37 +0100

    3.11 fixes

--- src/HABApp/core/const/loop.py
+++ src/HABApp/core/const/loop.py
@@ -1,5 +1,5 @@
 import asyncio
 
-loop = asyncio.get_event_loop_policy().get_event_loop()
+loop = asyncio.get_event_loop_policy().new_event_loop()
 loop.set_debug(True)
 loop.slow_callback_duration = 0.02
