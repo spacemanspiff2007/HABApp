e265d4972e08d11a25cdfb7706c68bcae6b2d313
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2023-02-09 08:01:01 +0100

    Updated splash screen

--- src/HABApp/__splash_screen__.py
+++ src/HABApp/__splash_screen__.py
@@ -1,8 +1,10 @@
+from colorama import just_fix_windows_console, deinit, Fore
+
 from HABApp.__version__ import __version__
 
 
 def show_screen():
-    txt = r"""
+    text = r"""
   _   _    _    ____    _
  | | | |  / \  | __ )  / \   _ __  _ __
  | |_| | / _ \ |  _ \ / _ \ | '_ \| '_ \
@@ -11,5 +13,23 @@ def show_screen():
                             |_|   |_|
 """
 
-    print(txt.strip('\n\r'))
+    red = r"""
+    _
+   / \   _ __  _ __
+  / _ \ | '_ \| '_ \
+ / ___ \| |_) | |_) |
+/_/   \_| .__/| .__/
+        |_|   |_|
+"""
+
+    just_fix_windows_console()
+
+    for line_text, line_red in zip(text.strip('\n\r').splitlines(), red.strip('\n\r').splitlines()):
+        red = line_red.strip()
+        assert line_text.endswith(red)
+        print(f'{line_text[:-len(red)]:s}{Fore.RED}{line_text[-len(red):]}{Fore.RESET}')
+
+    # Version nr
     print(f'{" " * 40}{__version__}')
+
+    deinit()
