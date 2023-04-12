0bc3cdafc7cfdfbe96b791036de7eedd7cb4059f
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2023-04-12 15:23:48 +0200

    Inspect HABApp

--- src/HABApp/core/internals/proxy/proxy_obj.py
+++ src/HABApp/core/internals/proxy/proxy_obj.py
@@ -18,7 +18,7 @@ class ProxyObjBase:
     def __call__(self, *args, **kwargs):
         raise ProxyObjHasNotBeenReplacedError(self)
 
-    def __str__(self):
+    def __repr__(self):
         return f'<{self.__class__.__name__} {self.to_replace_name}>'
 
 
@@ -29,6 +29,8 @@ class ConstProxyObj(ProxyObjBase):
     @property
     def to_replace_name(self) -> str:
         return self.name
+
+
 
 
 class StartUpProxyObj(ProxyObjBase):
