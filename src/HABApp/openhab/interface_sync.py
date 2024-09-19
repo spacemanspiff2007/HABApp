4b21d9396ba240f098b71ac508c2ad70a03de1f1
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2024-09-19 10:42:39 +0200

    .

---
+++ src/HABApp/openhab/interface_sync.py
@@ -0,0 +1,17 @@
+from HABApp.openhab.connection.handler.func_sync import (
+    create_item,
+    create_link,
+    get_item,
+    get_link,
+    get_persistence_data,
+    get_persistence_services,
+    get_thing,
+    item_exists,
+    remove_item,
+    remove_link,
+    remove_metadata,
+    set_metadata,
+    set_persistence_data,
+    set_thing_enabled,
+)
+from HABApp.openhab.connection.plugins import post_update, send_command
