71c251121e283e046827c3586a6a131c7832a7f7
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2024-11-25 16:11:58 +0100

    .

--- tests/test_openhab/test_items/test_commands.py
+++ tests/test_openhab/test_items/test_commands.py
@@ -13,7 +13,7 @@ from HABApp.openhab.map_items import _items as item_dict
 def test_OnOff(cls) -> None:
     c = cls('item_name')
     assert not c.is_on()
-    if not __version__.startswith('24.11.0'):
+    if not __version__.startswith('24.11.1'):
         assert not c.is_off()
 
     c.set_value(OnOffValue('ON'))
