b34221541a7fa6c2be5b95f3ad5d0b503d734fa8
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2025-03-26 14:54:27 +0100

    .

--- src/HABApp/core/connections/__init__.py
+++ src/HABApp/core/connections/__init__.py
@@ -16,4 +16,3 @@ from HABApp.core.connections.manager import connection_manager as Connections
 # isort: split
 
 from HABApp.core.connections.plugins import AutoReconnectPlugin, ConnectionStateToEventBusPlugin
-
