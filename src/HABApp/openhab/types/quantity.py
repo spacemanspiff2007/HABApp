d7b3422e91c7701105b1b3346c6ff0c6baf6c489
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2025-06-02 06:33:51 +0200

    Fixed issue this utility function and QuantityType

--- src/HABApp/openhab/types/quantity.py
+++ src/HABApp/openhab/types/quantity.py
@@ -8,7 +8,12 @@ class QuantityInt(int):
 
     unit: str   # unit as sent from openHAB
 
-    def __new__(cls, v: int, unit: str) -> Self:
+    def __new__(cls, v: int, unit: str | None = None) -> Self:
+        # Some utilities return a new type based on the input class type.
+        # Since these can't provide the unit we fall back to the native type
+        if unit is None:
+            return super().__new__(int, v)
+
         _obj = super().__new__(cls, v)
         _obj.unit = unit
         return _obj
@@ -24,7 +29,12 @@ class QuantityFloat(float):
 
     unit: str   # unit as sent from openHAB
 
-    def __new__(cls, v: float, unit: str) -> Self:
+    def __new__(cls, v: float, unit: str | None = None) -> Self:
+        # Some utilities return a new type based on the input class type.
+        # Since these can't provide the unit we fall back to the native type
+        if unit is None:
+            return super().__new__(float, v)
+
         _obj = super().__new__(cls, v)
         _obj.unit = unit
         return _obj
--- tests/test_openhab/test_types.py
+++ tests/test_openhab/test_types.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 from HABApp.openhab.types import RawType, StringList
 from HABApp.openhab.types.quantity import QuantityFloat, QuantityInt
 
@@ -11,26 +13,15 @@ def test_raw_text() -> None:
     assert str(t) == 'RawType(type=png data=0100000000..0000000002 (11kiB))'
 
 
-def test_raw_image() -> None:
-    t = RawType.create('image/svg+xml', b'\x01' + b'\x00' * 1_000 + b'\x02')
-    assert str(t) == 'RawType(type=svg+xml data=0100000000..0000000002 (1.0kiB))'
+def test_raw_type() -> None:
+    t = RawType.create('image/svg+xml', b'')
+    assert t.type == 'svg+xml'
 
-    b = b'\x01' + b'\x00' * 11_000 + b'\x02'
-    t = RawType.create('image/svg+xml', b)
-    assert str(t) == 'RawType(type=svg+xml data=0100000000..0000000002 (11kiB))'
-
-
-def test_raw_vendor_image() -> None:
-    t = RawType.create('image/vnd.clip', b'\x01' + b'\x00' * 1_000 + b'\x02')
-    assert str(t) == 'RawType(type=vnd.clip data=0100000000..0000000002 (1.0kiB))'
-
-    b = b'\x01' + b'\x00' * 11_000 + b'\x02'
-    t = RawType.create('image/vnd.clip', b)
-    assert str(t) == 'RawType(type=vnd.clip data=0100000000..0000000002 (11kiB))'
+    t = RawType.create('image/vnd.clip', b'')
+    assert t.type == 'vnd.clip'
 
 
 def test_raw() -> None:
-
     b = b'\x01\x02'
     t = RawType('image/png', b)
 
@@ -66,3 +57,21 @@ def test_quantity() -> None:
 
     assert QuantityFloat(1.3, 'asdf')._value_str() == '1.3 asdf'
     assert QuantityFloat(1.3, '')._value_str() == '1.3'
+
+    # test operator
+    e = i * QuantityInt(20_000, 'unit')
+    assert type(e) is int
+    e = f * QuantityFloat(20.0, 'unit')
+    assert type(e) is float
+
+    # Test conversion
+    # literals are 0..100 constants, this saves us the type check
+    assert int(i) is 1  # noqa: F632
+
+    # Test utility function which should work out of the box
+    result = mean([QuantityInt(10_000, 'u1'), QuantityInt(20_000, 'u2')])
+    assert result == 15_000
+    assert type(result) is int
+    result = mean([QuantityFloat(1.0, 'u1'), QuantityFloat(2.0, 'u2')])
+    assert result == 1.5
+    assert type(result) is float
