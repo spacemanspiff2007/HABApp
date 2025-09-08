6dfbb1e705087274ad4d64e222f9f3a12d0043c0
B Synnerlig <36725726+besynnerlig@users.noreply.github.com>
2025-09-08 15:14:21 +0100

    Make openhab.events reexports explicit and add __all__ for linter support

--- src/HABApp/openhab/events/__init__.py
+++ src/HABApp/openhab/events/__init__.py
@@ -1,33 +1,67 @@
-from .base_event import OpenhabEvent
-from .channel_events import ChannelDescriptionChangedEvent, ChannelTriggeredEvent
+from .base_event import OpenhabEvent as OpenhabEvent
+from .channel_events import (
+    ChannelDescriptionChangedEvent as ChannelDescriptionChangedEvent,
+    ChannelTriggeredEvent as ChannelTriggeredEvent,
+)
 from .item_events import (
-    GroupStateChangedEvent,
-    GroupStateUpdatedEvent,
-    ItemAddedEvent,
-    ItemCommandEvent,
-    ItemRemovedEvent,
-    ItemStateChangedEvent,
-    ItemStateEvent,
-    ItemStatePredictedEvent,
-    ItemStateUpdatedEvent,
-    ItemUpdatedEvent,
+    GroupStateChangedEvent as GroupStateChangedEvent,
+    GroupStateUpdatedEvent as GroupStateUpdatedEvent,
+    ItemAddedEvent as ItemAddedEvent,
+    ItemCommandEvent as ItemCommandEvent,
+    ItemRemovedEvent as ItemRemovedEvent,
+    ItemStateChangedEvent as ItemStateChangedEvent,
+    ItemStateEvent as ItemStateEvent,
+    ItemStatePredictedEvent as ItemStatePredictedEvent,
+    ItemStateUpdatedEvent as ItemStateUpdatedEvent,
+    ItemUpdatedEvent as ItemUpdatedEvent,
 )
 from .thing_events import (
-    ThingAddedEvent,
-    ThingConfigStatusInfoEvent,
-    ThingFirmwareStatusInfoEvent,
-    ThingRemovedEvent,
-    ThingStatusInfoChangedEvent,
-    ThingStatusInfoEvent,
-    ThingUpdatedEvent,
+    ThingAddedEvent as ThingAddedEvent,
+    ThingConfigStatusInfoEvent as ThingConfigStatusInfoEvent,
+    ThingFirmwareStatusInfoEvent as ThingFirmwareStatusInfoEvent,
+    ThingRemovedEvent as ThingRemovedEvent,
+    ThingStatusInfoChangedEvent as ThingStatusInfoChangedEvent,
+    ThingStatusInfoEvent as ThingStatusInfoEvent,
+    ThingUpdatedEvent as ThingUpdatedEvent,
 )
-
 
 # isort: split
 
 from .event_filters import (
-    ItemCommandEventFilter,
-    ItemStateChangedEventFilter,
-    ItemStateEventFilter,
-    ItemStateUpdatedEventFilter,
+    ItemCommandEventFilter as ItemCommandEventFilter,
+    ItemStateChangedEventFilter as ItemStateChangedEventFilter,
+    ItemStateEventFilter as ItemStateEventFilter,
+    ItemStateUpdatedEventFilter as ItemStateUpdatedEventFilter,
 )
+
+__all__ = [
+    # base
+    "OpenhabEvent",
+    # channel events
+    "ChannelDescriptionChangedEvent",
+    "ChannelTriggeredEvent",
+    # item events
+    "GroupStateChangedEvent",
+    "GroupStateUpdatedEvent",
+    "ItemAddedEvent",
+    "ItemCommandEvent",
+    "ItemRemovedEvent",
+    "ItemStateChangedEvent",
+    "ItemStateEvent",
+    "ItemStatePredictedEvent",
+    "ItemStateUpdatedEvent",
+    "ItemUpdatedEvent",
+    # thing events
+    "ThingAddedEvent",
+    "ThingConfigStatusInfoEvent",
+    "ThingFirmwareStatusInfoEvent",
+    "ThingRemovedEvent",
+    "ThingStatusInfoChangedEvent",
+    "ThingStatusInfoEvent",
+    "ThingUpdatedEvent",
+    # filters
+    "ItemCommandEventFilter",
+    "ItemStateChangedEventFilter",
+    "ItemStateEventFilter",
+    "ItemStateUpdatedEventFilter",
+]
