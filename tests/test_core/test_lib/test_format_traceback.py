74b981bd4d3df0fa50bc2fe8687b49ecd0517697
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2026-07-15 08:09:29 +0200

    ci

--- tests/test_core/test_lib/test_format_traceback.py
+++ tests/test_core/test_lib/test_format_traceback.py
@@ -170,6 +170,7 @@ def func_ir() -> None:
 
 @pytest.mark.skipif(not PYTHON_313, reason='New traceback from python 3.13')
 def test_skip_objs(ir) -> None:
+    """Checks that the dict with all the items is not shown in the traceback"""
     log.setLevel(logging.WARNING)
     msg = exec_func(func_ir)
     print('\n\n-')
@@ -183,32 +184,30 @@ File "test_core/test_lib/test_format_traceback.py", line x in exec_func
 -->  x |         func()
      x |     except Exception as e:
    ------------------------------------------------------------
-     e = ObjectDoesNotExistError('Object for ItemRegistry does not exist')
+     e = ItemNotFoundException('Item 1234 does not exist!')
      func = <function func_ir at 0xAAAAAAAAAAAAAAAA>
    ------------------------------------------------------------
 
 File "test_core/test_lib/test_format_traceback.py", line x in func_ir
 --------------------------------------------------------------------------------
      x | def func_ir() -> None:
--->  x |     items: Final = HABAPP_PROVIDER.get_existing(ItemRegistry)
-     x |     items.get_item('1234')
+     x |     items: Final = HABAPP_PROVIDER.get_existing(ItemRegistry)
+-->  x |     items.get_item('1234')
    ------------------------------------------------------------
      HABAPP_PROVIDER = <HABApp.core.provider.provider.HabAppObjProvider object at 0xAAAAAAAAAAAAAAAA>
+     items = <HABApp.core.internals.item_registry.item_registry.ItemRegistry object at 0xAAAAAAAAAAAAAAAA>
    ------------------------------------------------------------
 
-File "core/provider/provider.py", line x in get_existing
+File "internals/item_registry/item_registry.py", line x in get_item
 --------------------------------------------------------------------------------
-     x | def get_existing(self, cls: type[T]) -> T:
-       (...)
-     x |         return self._created[cls]
+     x | def get_item(self, name: str) -> ItemRegistryItem:
+     x |     try:
+     x |         return self._items[name]
      x |     except KeyError:
-     x |         msg = f'Object for {cls.__name__} does not exist'
--->  x |         raise ObjectDoesNotExistError(msg) from None
+-->  x |         raise ItemNotFoundException(name) from None
    ------------------------------------------------------------
-     cls = <class 'HABApp.core.internals.item_registry.item_registry.ItemRegistry'>
-     self = <HABApp.core.provider.provider.HabAppObjProvider object at 0xAAAAAAAAAAAAAAAA>
-     self._created = {<class 'HABApp.core.provider.provider.HabAppObjProvider'>: <HABApp.core.provider.provider.HabAppObjProvider object at 0xAAAAAAAAAAAAAAAA>, <class 'HABApp.core.items.base_item_times_data.ItemTimesBackup'>: <Mock spec='ItemTimesBackup' id='140147242873728'>}
-     msg = 'Object for ItemRegistry does not exist'
+     name = '1234'
+     self = <HABApp.core.internals.item_registry.item_registry.ItemRegistry object at 0xAAAAAAAAAAAAAAAA>
    ------------------------------------------------------------
 
 --------------------------------------------------------------------------------
@@ -217,11 +216,11 @@ Traceback (most recent call last):
     func()
     ~~~~^^
   File "test_core/test_lib/test_format_traceback.py", line x, in func_ir
-    items: Final = HABAPP_PROVIDER.get_existing(ItemRegistry)
-                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^
-  File "core/provider/provider.py", line x, in get_existing
-    raise ObjectDoesNotExistError(msg) from None
-HABApp.core.provider.provider.ObjectDoesNotExistError: Object for ItemRegistry does not exist'''
+    items.get_item('1234')
+    ~~~~~~~~~~~~~~^^^^^^^^
+  File "internals/item_registry/item_registry.py", line x, in get_item
+    raise ItemNotFoundException(name) from None
+HABApp.core.errors.ItemNotFoundException: Item 1234 does not exist!'''
 
 
 def multiline_obj_name() -> None:
