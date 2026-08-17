60b54de7cc6458e63127abdc1f3dca78a407ca90
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2026-08-17 07:25:15 +0200

    .

--- run/conf_testing/rules/habapp/test_rule_context.py
+++ run/conf_testing/rules/habapp/test_rule_context.py
@@ -1,8 +1,10 @@
+import threading
 from time import sleep
 
 from HABAppTests import TestBaseRule
 
 from HABApp import Rule
+from HABApp.core.errors import ContextNotFoundError
 from HABApp.core.internals import get_current_context
 
 
@@ -33,6 +35,8 @@ class TestContext(TestBaseRule):
         self.add_test('TestGetContext', self.test_get_context)
         self.add_test('TestSchedulerContext', self.test_scheduler_context)
         self.add_test('TestSchedulerAsyncContext', self.test_scheduler_async_context)
+        self.add_test('TestUserThreadContext', self.test_user_thread_context)
+        self.add_test('TestUserThreadRuleContext', self.test_user_thread_rule_context)
 
     def test_get_context(self) -> None:
         assert get_current_context() is self._habapp_ctx
@@ -47,6 +51,33 @@ class TestContext(TestBaseRule):
         self.run.soon(async_func_no_context, self._habapp_ctx)
         self.run.soon(other_rule.async_check_context)
         sleep(0.1)
+
+    def test_user_thread_context(self) -> None:
+
+        objs = []
+
+        def thread_func() -> None:
+            try:
+                cxt = get_current_context()
+            except Exception as e:
+                ctx = e
+
+            objs.append(ctx)
+
+        t = threading.Thread(target=thread_func)
+        t.start()
+        t.join()
+
+        # The context should not leak into the thread
+        assert len(objs) == 1
+        assert isinstance(objs[0], ContextNotFoundError)
+
+    def test_user_thread_rule_context(self) -> None:
+
+        # user calls rule from a thread - should work as expected
+        t = threading.Thread(target=self.test_get_context)
+        t.start()
+        t.join()
 
 
 TestContext()
